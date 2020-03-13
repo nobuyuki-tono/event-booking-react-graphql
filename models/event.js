3dbceff09098250000f94220629f7754655a96b5
Nobu <rockmusic2090@gmail.com>
2020-03-13 13:42:03 -0700

    Created event model

---
+++ models/event.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required
+  }
+});
+
+module.exports = Event = mongoose.model("grapgql-event", eventSchema);
