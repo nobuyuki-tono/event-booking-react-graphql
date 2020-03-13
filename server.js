671b9aae5846db50cc694235bc37ed6b91ae9f14
Nobu <rockmusic2090@gmail.com>
2020-03-12 17:19:38 -0700

    Added endpoint for graphql

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+       events: [String!]!
+    }
+
+    type RootMutation {
+       createEvent(name: String) : String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutaion
+    }
+  `),
+    rootValue: {}
+  })
+);
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
