17acc7b1cc9bc68e4a26bbcfc58eb871fdffc51a
Nobu <rockmusic2090@gmail.com>
2020-03-13 13:11:02 -0700

    Added README file

--- server.js
+++ server.js
@@ -4,26 +4,60 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = [];
+
 app.use(express.json());
 
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-       events: [String!]!
+       events: [Event!]!
     }
 
     type RootMutation {
-       createEvent(name: String) : String
+       createEvent(eventInput: EventInput) : Event
     }
 
     schema {
       query: RootQuery
-      mutation: RootMutaion
+      mutation: RootMutation
     }
   `),
-    rootValue: {}
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: args => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
+      }
+    },
+    graphiql: true
   })
 );
 
